a5c85fdcef45f9433d42d54f1df782afb33522c6
MichalDawidowicz <mpd2001pl@gmail.com>
2024-04-05 13:07:06 +0200

    rejestracja

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -2,17 +2,43 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ogloszenia/components/my_button.dart';
 import 'package:ogloszenia/components/my_textfield.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:ogloszenia/helper/helper_functions.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   final void Function()? onTap;
-  //text controllers
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
 
   RegisterPage({super.key, this.onTap});
 
-  void register(){
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
 
+class _RegisterPageState extends State<RegisterPage> {
+  //text controllers
+  final TextEditingController emailController = TextEditingController();
+
+  final TextEditingController passwordController = TextEditingController();
+
+  Future<void> register() async {
+    showDialog(
+      context: context,
+      builder: (context) => const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+    //try to create user
+    try {
+      //create
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text);
+      //kolo
+      Navigator.pop(context);
+    } on FirebaseAuthException catch (e) {
+      Navigator.pop(context);
+      displayMessageToUser(e.code, context);
+    }
   }
 
   @override
@@ -31,13 +57,22 @@ class RegisterPage extends StatelessWidget {
             ),
             const SizedBox(height: 25),
             //app name
-            Text("PIESKOWE OGŁOSZENIA", style: TextStyle(fontSize: 20),),
+            Text(
+              "PIESKOWE OGŁOSZENIA",
+              style: TextStyle(fontSize: 20),
+            ),
             const SizedBox(height: 25),
             //email textfield
-            MyTextField(hintText: "Email", obscureText: false, controller: emailController),
+            MyTextField(
+                hintText: "Email",
+                obscureText: false,
+                controller: emailController),
             const SizedBox(height: 11),
             //password textfield
-            MyTextField(hintText: "Hasło", obscureText: true, controller: passwordController),
+            MyTextField(
+                hintText: "Hasło",
+                obscureText: true,
+                controller: passwordController),
             const SizedBox(height: 22),
             //zarejestruj
             MyButton(text: "Zarejestruj", onTap: register),
@@ -48,10 +83,13 @@ class RegisterPage extends StatelessWidget {
               children: [
                 Text("Mam już konto "),
                 GestureDetector(
-                    onTap: onTap,
-                    child: Text(" Zaloguj się",style: TextStyle(fontWeight: FontWeight.bold),))
-              ],)
-
+                    onTap: widget.onTap,
+                    child: Text(
+                      " Zaloguj się",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ))
+              ],
+            )
           ]),
         ),
       ),
