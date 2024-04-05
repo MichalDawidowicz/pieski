46943e00dbee50173c88a9d1349a290813b0c06b
MichalDawidowicz <mpd2001pl@gmail.com>
2024-04-05 12:49:00 +0200

    first commit

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ogloszenia/components/my_button.dart';
+import 'package:ogloszenia/components/my_textfield.dart';
+
+class RegisterPage extends StatelessWidget {
+  final void Function()? onTap;
+  //text controllers
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  RegisterPage({super.key, this.onTap});
+
+  void register(){
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            // logo
+            Icon(
+              Icons.accessibility,
+              size: 80,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            const SizedBox(height: 25),
+            //app name
+            Text("PIESKOWE OGŁOSZENIA", style: TextStyle(fontSize: 20),),
+            const SizedBox(height: 25),
+            //email textfield
+            MyTextField(hintText: "Email", obscureText: false, controller: emailController),
+            const SizedBox(height: 11),
+            //password textfield
+            MyTextField(hintText: "Hasło", obscureText: true, controller: passwordController),
+            const SizedBox(height: 22),
+            //zarejestruj
+            MyButton(text: "Zarejestruj", onTap: register),
+            const SizedBox(height: 11),
+            //logowanie
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Mam już konto "),
+                GestureDetector(
+                    onTap: onTap,
+                    child: Text(" Zaloguj się",style: TextStyle(fontWeight: FontWeight.bold),))
+              ],)
+
+          ]),
+        ),
+      ),
+    );
+  }
+}
